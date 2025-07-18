b76ee22c5298c8cb4ec10c46bbd348564d8cf0b5
li <lishunyang12@163.com>
2025-07-18 20:52:05 +0800

    fiber interface defined

---
+++ sylar/sylar/scheduler.cc
@@ -0,0 +1,28 @@
+#include "scheduler.h"
+#include "log.h"
+
+namespace sylar {
+static sylar::Logger::ptr g_logger = SYSLAR_LOG_NAME("system");
+
+Scheduler::Scheduler(size_t threads, bool use_caller, const std::string& name) {
+
+}
+
+Scheduler::~Scheduler() {
+
+}
+
+Scheduler* Scheduler::GetThis() {
+
+}
+
+Fiber* Scheduler::GetMainFiber() {
+
+}
+
+void Scheduler::stop() {
+
+}
+
+
+}
---
+++ sylar/sylar/scheduler.h
@@ -0,0 +1,110 @@
+#ifndef __SYLAR_SCHDULER_H__
+#define __SYLAR_SCHDULER_H__
+
+#include <memory>
+#include <vector>
+#include <list>
+#include "fiber.h"
+#include "thread.h"
+#include "fiber.h"
+
+namespace sylar {
+
+class Scheduler {
+public:
+    typedef std::shared_ptr<Scheduler> ptr;
+    typedef Mutex MutexType;
+
+    Scheduler(size_t threads = 1, bool use_caller = true, const std::string& name = "");
+    virtual ~Scheduler();
+    
+    const std::string& getName() const { return m_name; }
+
+    static Scheduler* GetThis();
+    static Fiber* GetMainFiber();
+
+    void start();
+    void stop();
+
+    template<class FiberOrCb> 
+    void schedule(FiberOrCb fc, int thread = -1) {
+        bool need_tickle = false;
+        {
+            MutexType::Lock lock(m_mutex);
+            need_tickle = scheduleNoLock(fc, thread);
+        }
+        
+        if(need_tickle) {
+            tickle();
+        }
+    }
+    
+    template<class InputIterator> 
+    void schedule(InputIterator begin, InputIterator end) {
+        bool need_tickle = false;
+        {
+            MutexType::lock lock(m_mutex);
+            while(begin != end) {
+                tickle = scheduleNoLock(&*begin) || need_tickle;
+            }
+        }
+        if(need_tickle) {
+            tickle();
+        }
+    }
+protected:
+    virtual void tickle();
+private:
+    template<class FiberOrCb>
+    bool scheduleNoLock(FiberOrCb fc, int thread) {
+        bool need_tickle = m_fiber.empty();
+        FiberAndThread ft(fc, thread);
+        if(ft.fiber || ft.cb) {
+            m_fiber.push_back(ft);
+        }
+        return need_tickle;
+    }
+private:
+    struct FiberAndThread {
+        Fiber::ptr fiber;
+        std::function<void()> cb;
+        int thread;
+
+        FiberAndThread(Fiber::ptr f, int thr)
+            :fiber(f), thread(thr) {}
+        
+        FiberAndThread(Fiber::ptr* f, int thr) 
+            :thread(thr) {
+                fiber.swap(*f);     // convert to nullptr and decrement count
+        }
+
+        FiberAndThread(std::function<void()> f, int thr) 
+            :cb(f), thread(thr) {   
+        }
+
+        FiberAndThread(std::function<void()>* f, int thr)
+            :thread(thr) {
+                cb.swap(*f);
+        }
+
+        FiberAndThread() 
+            :thread(-1) {
+        }
+
+        void reset() {
+            fiber = nullptr;
+            cb = nullptr;
+            thread = -1;
+        }
+    };
+
+private:
+    MutexType m_mutex;
+    std::vector<Thread::ptr> m_threads;
+    std::list<FiberAndThread> m_fiber;
+    std::string m_name;
+};
+
+}
+
+#endif
